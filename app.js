3aef1c233a372b984faaa5673a39bf4f43400862
Rogelio <roger.cruz808@gmail.com>
2023-11-13 10:33:47 -0800

    Previous answers feature

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function play() {
     let secretNumber = "5";
     let attempts = 1;
+    let tries = []
 
     while(true) {
         const guess = prompt("Guess a number!");
@@ -10,7 +11,7 @@ function play() {
         }
         if (guess == secretNumber) {
             alert("You win!")
-            prompt(`It took you ${attempts} attempts!`)
+            prompt(`It took you ${attempts} attempts! Your previous answers were ${tries}. The correct answer was ${secretNumber}.`)
             break;
         }
         if (guess > secretNumber) {
@@ -19,6 +20,7 @@ function play() {
         if (guess < secretNumber) {
             alert("Higher! Try again!");
         }
+        tries.push(attempts)
         attempts += 1
     }
 }

85307f186e8d42653b306520be88e5a5d55c3e69
Rogelio <roger.cruz808@gmail.com>
2023-11-13 09:59:57 -0800

    feature 1 added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function play() {
-    let secretNumber = 5;
+    let secretNumber = "5";
+    // let secretNumber = 5;
 
     while(true) {
         const guess = prompt("Guess a number!");
@@ -10,6 +11,12 @@ function play() {
         if (guess == secretNumber) {
             alert("You win!")
             break;
+        }
+        if (guess > secretNumber) {
+            alert("Guess lower!");
+        }
+        if (guess < secretNumber) {
+            alert("Guess higher!");
         }
     }
 }

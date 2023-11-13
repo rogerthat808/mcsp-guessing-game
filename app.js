b5cb9e913dea80b513800ee9c6645ec4818b8974
Rogelio <roger.cruz808@gmail.com>
2023-11-13 15:56:17 -0800

    working on feature 7

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+// function play is created
 function play() {
     let secretNumber = "5";
     let attempts = 1;
     let tries = []
     let name;
-    
-    // Below is a function checking to ensure a name has been inputted.
+
+// Below is a function checking to ensure a NAME has been inputted.
     function nameCheck() {
     name = prompt("What is your name?");
     }
     
-    // Below is a function asking the user if he'd like to play again.
+// Below is a function asking the user if he'd like to PLAY AGAIN.
     function playAgain() {
         let answer = prompt("want to play again? Y / N")
         if (answer == "Y" || answer == "y") {
@@ -20,13 +21,15 @@ function play() {
         }
     }
 
-    // Start of the loop
+// Start of the loop explaining possible inputs and outputs
     while(true) {
+// loop ensures nameCheck function is invoked if the variable name isn't given a true value.
         while (name == undefined || name == null | name == "") {
             nameCheck()
         }
+// Using prompt to assign the guess variable a value
         const guess = prompt(`${name}, guess a number?`);
-        debugger;
+
         if (guess === null || guess === "") {
             alert("Goodbye!")
             break;
@@ -49,9 +52,11 @@ function play() {
         if (guess < secretNumber) {
             alert(`${name}, guess higher! Try again.`);
         }
+// past tries get moved to tries array and number of attempts is increased
         tries.push(guess)
         attempts += 1
     }
 }
-
+// start the game by invoking the function
 play()
+

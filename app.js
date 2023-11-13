eff729f751e0d765882fd2f8ab9d81c3e1762bf3
Rogelio <roger.cruz808@gmail.com>
2023-11-13 13:43:29 -0800

    Feature 6 added

--- app.js
+++ app.js
@@ -2,18 +2,45 @@ function play() {
     let secretNumber = "5";
     let attempts = 1;
     let tries = []
-
-    const name = prompt("What is your name?")
-
-    while(true) {
-        const guess = prompt(`${name}, guess a number?`);
-        if (guess === null) {
+    let name;
+    
+    // Below is a function checking to ensure a name has been inputted.
+    function nameCheck() {
+    name = prompt("What is your name?");
+    }
+    
+    // Below is a function asking the user if he'd like to play again.
+    function playAgain() {
+        let answer = prompt("want to play again? Y / N")
+        if (answer == "Y" || answer == "y") {
+            play()
+        }
+        if (answer == "N" || answer == "n") {
             alert("Goodbye!")
+        }
+    }
+
+    // Start of the loop
+    while(true) {
+        while (name == undefined || name == null | name == "") {
+            nameCheck()
+        }
+        const guess = prompt(`${name}, guess a number?`);
+        debugger;
+        if (guess === null || guess === "") {
+            alert("Goodbye!")
+            break;
+        }
+        if (guess == secretNumber && attempts == 1) {
+            alert(`Congrats ${name}, you won!`);
+            alert(`${name}, It took you 1 attempt! Your answer was ${secretNumber}.`)
+            playAgain()
             break;
         }
         if (guess == secretNumber) {
             alert(`Congrats ${name}, you won!`)
             alert(`${name}, It took you ${attempts} attempts! Your previous answers were ${tries}. The correct answer was ${secretNumber}.`)
+            playAgain()
             break;
         }
         if (guess > secretNumber) {
